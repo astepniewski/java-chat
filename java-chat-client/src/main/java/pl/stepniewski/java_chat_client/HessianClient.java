ee4d6918ea7ad1d029a709cfe7e6f8249b4910d6
Arek Stępniewski <Arek Stpniewski@10.9.128.106>
2017-01-05 18:10:04 +0100

    Modified client ui.

--- java-chat-client/src/main/java/pl/stepniewski/java_chat_client/HessianClient.java
+++ java-chat-client/src/main/java/pl/stepniewski/java_chat_client/HessianClient.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
@@ -14,34 +16,36 @@ import pl.stepniewski.java_chat_contracts.CommunicationService;
 
 public class HessianClient {
 
-    public static void main(String[] args) throws Exception {
-        String url = "http://localhost:8080/communication-service";
-        HessianProxyFactory factory = new HessianProxyFactory();
-        final CommunicationService basic = (CommunicationService) factory.create(CommunicationService.class, url);
+	public static void main(String[] args) throws Exception {
+		String url = "http://localhost:8080/communication-service";
+		HessianProxyFactory factory = new HessianProxyFactory();
+		final CommunicationService basic = (CommunicationService) factory.create(CommunicationService.class, url);
 
-        SwingUtilities.invokeLater(new Runnable() {
+		SwingUtilities.invokeLater(new Runnable() {
 
-            public void run() {
-                JFrame frame = new JFrame("Hessian Hello World");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setLayout(new BorderLayout());
-                final JTextField field = new JTextField("Peter", 100);
-                frame.add(field, BorderLayout.NORTH);
-                field.setToolTipText("Press Enter to submit text to server");
-                
-                final JLabel resultLabel = new JLabel("Server Response");
-                frame.add(resultLabel, BorderLayout.SOUTH);
+			public void run() {
+				JFrame frame = new JFrame("Hessian Hello World");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setLayout(new BorderLayout());
+				final JTextField field = new JTextField("Peter", 100);
+				frame.add(field, BorderLayout.SOUTH);
+				field.setToolTipText("Press Enter to submit text to server");
 
-                field.addActionListener(new ActionListener() {
+				final JTextArea resultArea = new JTextArea();
+				JScrollPane scroll = new JScrollPane(resultArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+						JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+				frame.add(scroll, BorderLayout.CENTER);
 
-                    public void actionPerformed(ActionEvent e) {
-                        resultLabel.setText("Server said : " + basic.communicate(field.getText()));
-                    }
-                });
+				field.addActionListener(new ActionListener() {
 
-                frame.setSize(600, 400);
-                frame.setVisible(true);
-            }
-        });
-    }
+					public void actionPerformed(ActionEvent e) {
+						resultArea.append("Server said : " + basic.communicate(field.getText()) + "\n");
+					}
+				});
+
+				frame.setSize(600, 400);
+				frame.setVisible(true);
+			}
+		});
+	}
 }
