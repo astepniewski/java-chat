c6e90b887941ef8f94676946840f5823b12b78fb
Arek Stępniewski <Arek Stpniewski@10.9.128.106>
2017-01-05 17:24:32 +0100

    Rename server project. Add empty class of Hessian Server.

---
+++ java-chat/src/main/java/pl/stepniewski/java_chat/HessianServer.java
@@ -0,0 +1,12 @@
+package pl.stepniewski.java_chat;
+
+import com.caucho.hessian.server.HessianServlet;
+
+public class HessianServer extends HessianServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
